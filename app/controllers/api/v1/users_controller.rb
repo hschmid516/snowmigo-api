88d2b5731eeb9b47c762a9b50dbf23cfd1f5498e
Ozzie <mikeosmonson@gmail.com>
2021-11-03 11:47:03 -0600

    Add Users controller #show #create #update #destroy w/tests to create endpoints

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,32 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController < Api::V1::Users::BaseController
+  before_action only: %i[show update destroy] do
+    @user = set_user
+  end
+
   def index
   end
 
   def show
+    serialize(@user)
   end
 
   def create
+    new_user = User.create!(user_params)
+    serialize(new_user, :created)
   end
 
   def update
+    @user.update!(user_params)
+    serialize(@user)
   end
 
   def destroy
+    user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :exp_level, :ski_pass, :address, :ski_or_board, :emergency_name, :emergency_number)
   end
 end
