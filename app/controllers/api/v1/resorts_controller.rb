cd672515e4c648d86a30a751b733ecd7952bc660
Henry Schmid <henry.schmid1@gmail.com>
2021-11-04 20:49:48 -0600

    Add show action and update index

--- app/controllers/api/v1/resorts_controller.rb
+++ app/controllers/api/v1/resorts_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::ResortsController < ApplicationController
   def index
-    resorts = ResortFacade.get_resorts_by_state(params[:states])
-    render json: StateResortSerializer.new(resorts)
+    if params[:states]
+      resorts = ResortFacade.resorts_by_state(params[:states])
+      render json: StateResortSerializer.new(resorts)
+    elsif params[:ids]
+      resorts = ResortFacade.resorts_by_id(params[:ids])
+      render json: ResortSerializer.new(resorts)
+    end
+  end
+
+  def show
+    resorts = ResortFacade.resorts_by_id(params[:id])
+    render json: ResortSerializer.new(resorts)
   end
 end
