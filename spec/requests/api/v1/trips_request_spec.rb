ed405f4cfe6a8c528e1979a4913a09970b586223
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-04 21:00:00 -0700

    Complete list of trips spec

--- spec/requests/api/v1/trips_request_spec.rb
+++ spec/requests/api/v1/trips_request_spec.rb
@@ -6,21 +6,37 @@ RSpec.describe 'Trips' do
       @user = create(:user)
       @user2 = create(:user)
       @user3 = create(:user)
-      @trip = create(:trip)
-      @trip1 = create(:trip)
-      @trips = create_list(:trip, 10)
-      @riders = create(:rider, trip: trip, user: user)
-      @riders = create(:rider, trip: trip, user: user2)
-      @riders = create(:rider, trip: trip1, user: user3)
-      @riders = create(:rider, trip: trip1, user: user2)
-      @riders = create_list(:rider, 10)
+      @resort = Resort.new({id: 1, resortName: 'Big Boi Mountain'})
+      @resort2 = Resort.new({id: 2, resortName: 'Small Gorl Mountain'})
+      # @trip = create(:trip, resort_id: @resort.id)
+      # @trip1 = create(:trip, resort_id: @resort2.id)
+      @trips = create_list(:trip, 10, resort_id: @resort.id)
+      @riders = create(:rider, trip: @trips[0], user: @user)
+      @riders2 = create(:rider, trip: @trips[1], user: @user2)
+      @riders3 = create(:rider, trip: @trips[1], user: @user3)
+      @riders4 = create(:rider, trip: @trips[2], user: @user2)
+      # @riders5 = create_list(:rider, 10)
     end
 
     describe 'Index Endpoint' do
       it 'returns a list of Trips' do
         get '/api/v1/trips', params: { user_id: 1 }
+
+        expect(response).to be_successful
+
+        expect(json).to be_a(Hash)
+
+        expect(json[:data]).to be_a(Array)
+        expect(json[:data].count).to eq(10)
         
-        expect(request).to be_successful
+        json[:data] do |resource|
+          expect(resource[:id]).to be_a(String)
+          expect(resource[:type]).to eq('trip')
+          expect(resource[:attributes]).to be_a(Hash)
+          expect(resourse[:attributes][:resort_id]).to be_a(String)
+          expect(resourse[:attributes][:start_date]).to be_a(String)
+          expect(resourse[:attributes][:end_date]).to be_a(String)
+        end
       end
     end
   end
