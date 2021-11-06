ff8769b7768745972312fe312bf0eb338271d406
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-06 14:18:48 -0700

    Add name to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_174836) do
+ActiveRecord::Schema.define(version: 2021_11_06_195742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_11_03_174836) do
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
