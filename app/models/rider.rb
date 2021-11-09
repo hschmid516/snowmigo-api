7d0b4ee5721e206622abf3968a73dfb96c90e6be
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-09 13:37:37 -0700

    Add by user and trip class method

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,4 +1,8 @@
 class Rider < ApplicationRecord
   belongs_to :trip
   belongs_to :user
+
+  def self.by_user_and_trip(user_id, trip_id)
+    where("user_id = ? AND trip_id = ?", user_id, trip_id).first
+  end
 end
