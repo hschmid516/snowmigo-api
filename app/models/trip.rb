adf24728f0ceec8449b6683acef6046f89b8988a
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-09 00:58:42 -0700

    Add update resort votes method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,10 @@
 class Trip < ApplicationRecord
   has_many :resort_options, dependent: :destroy
   has_many :riders, dependent: :destroy
+
+  def update_resort_votes
+    resort_options.map do |resort_opt|
+      resort_opt.update(vote_count: resort_opt.counted_votes)
+    end
+  end
 end
