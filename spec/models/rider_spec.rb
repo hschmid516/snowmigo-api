e43b6652aa56d7acbd9dc172c017ec6e242e9cf8
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-09 13:36:59 -0700

    Add test for by user and trip class method

--- spec/models/rider_spec.rb
+++ spec/models/rider_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Rider, type: :model do
-  it { should belong_to(:trip) }
-  it { should belong_to(:user) }
+  describe 'relationships' do
+    it { should belong_to(:trip) }
+    it { should belong_to(:user) }
+  end
+
+  describe 'class methods' do
+    it '#by_trip_and_user' do
+      trips = create_list(:trip, 2)
+      users = create_list(:user, 2)
+      rider = create(:rider, trip: trips[0], user: users[1])
+
+      expect(Rider.by_trip_and_user(users[1].id, trips[0].id)).to eq(rider)
+    end
+  end
 end
