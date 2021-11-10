a3a891449c4bd3de83730f94995930e45516a101
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-10 00:39:04 -0700

    Add friend_name method

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,8 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
   belongs_to :user
   validates_uniqueness_of :friend_id, scope: :user_id
+
+  def friend_name
+    friend.name
+  end
 end
