aff910133fd43ac1ab15c324530866aaf60b8e65
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-10 14:31:19 -0700

    Add host rider expectations to test

--- spec/requests/api/v1/trips_request_spec.rb
+++ spec/requests/api/v1/trips_request_spec.rb
@@ -84,8 +84,9 @@ RSpec.describe 'Trips' do
 
       end
 
-      it 'can create a trip without a resort' do
+      it 'can create a trip and hosting rider without a resort' do
         trip_params = {
+          user_id: @users[0].id,
           name: 'Teehee Trip',
           start_date: '10/11/2021',
           end_date: '14/11/2021'
@@ -104,13 +105,16 @@ RSpec.describe 'Trips' do
         expect(json[:data][:attributes][:end_date]).to be_a(String)
         expect(json[:data][:attributes][:vote_status]).to be_a(String)
         expect(json[:data][:attributes][:resort_options].empty?).to eq(true)
+        expect(json[:data][:attributes][:riders].first[:data][:attributes][:user_id]).to eq(@users[0].id)
+        expect(json[:data][:attributes][:riders].first[:data][:attributes][:host]).to be_a(TrueClass)
 
         expect(json[:data][:attributes]).to_not have_key(:created_at)
         expect(json[:data][:attributes]).to_not have_key(:updated_at)
       end
 
-      it 'can create a trip with a resort' do
+      it 'can create a trip and hosting rider with a resort' do
         trip_params = {
+          user_id: @users[0].id,
           name: 'Resort Trip',
           start_date: '10/11/2021',
           resort_id: 1,
@@ -120,6 +124,8 @@ RSpec.describe 'Trips' do
         post '/api/v1/trips', params: trip_params
         expect(response).to be_successful
         expect(json[:data][:attributes][:resort_options].first[:data][:attributes][:resort_id]).to eq(1)
+        expect(json[:data][:attributes][:riders].first[:data][:attributes][:user_id]).to eq(@users[0].id)
+        expect(json[:data][:attributes][:riders].first[:data][:attributes][:host]).to be_a(TrueClass)
       end
 
       it 'can update a trip' do
