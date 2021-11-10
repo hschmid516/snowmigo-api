aed08cf071986dce9717b334e82ce903b36a3454
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-10 13:44:58 -0700

    Add open vote functionality to controller

--- app/controllers/api/v1/trips/votes_controller.rb
+++ app/controllers/api/v1/trips/votes_controller.rb
@@ -12,15 +12,31 @@ class Api::V1::Trips::VotesController < Api::V1::Trips::BaseController
 
   def update_status
     if params[:open] == 'false'
-      trip = Trip.find(params[:trip_id])
-      trip.update(vote_status: 'closed', resort_id: trip.max_vote_resort)
+      close_vote
+    elsif params[:open] == 'true'
+      open_vote
+    else
+      bad_request
     end
-    serialize(trip)
   end
 
   private
 
   def valid_params
     params[:user_id].present? && params[:resort_id].present?
+  end
+
+  def open_vote
+    trip = Trip.find(params[:trip_id])
+    trip.resort_options.create({ resort_id: trip.resort_id, resort_name: trip.resort_name})
+    trip.update({vote_status: 'open', resort_id: nil, resort_name: nil})
+    serialize(trip)
+  end
+
+  def close_vote
+    trip = Trip.find(params[:trip_id])
+    trip.update(vote_status: 'closed', resort_id: trip.max_vote_resort)
+    trip.resort_options.destroy_all
+    serialize(trip)
   end
 end
