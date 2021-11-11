38a8938ad72c15aae4a3b9b676f7f8e48804622e
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-11 01:01:26 -0700

    Adjust test to send email in params

--- spec/requests/api/v1/riders_request_spec.rb
+++ spec/requests/api/v1/riders_request_spec.rb
@@ -9,11 +9,11 @@ describe 'riders' do
       it 'can create riders' do
         rider_params = {
           trip_id: trip.id,
-          user_id: user.id,
+          email: user.email,
           driver: false,
           budget: 300
         }
-        post "/api/v1/riders", params: { rider: rider_params }
+        post "/api/v1/riders", params: rider_params
         rider = Rider.last
 
         expect(response).to have_http_status(:created)
@@ -28,7 +28,7 @@ describe 'riders' do
         expect(json[:data][:attributes][:driver]).to be_a(FalseClass)
         expect(json[:data][:attributes][:budget]).to be_an(Integer)
         expect(rider.trip_id).to eq(rider_params[:trip_id])
-        expect(rider.user_id).to eq(rider_params[:user_id])
+        expect(rider.user_id).to eq(user.id)
         expect(rider.driver).to eq(false)
         expect(rider.budget).to eq(300)
       end
@@ -42,33 +42,33 @@ describe 'riders' do
     it '400 if no trip found' do
       rider_params = {
         trip_id: 75679678,
-        user_id: @user.id,
+        email: @user.email,
         driver: false,
         budget: 300
       }
-      post "/api/v1/riders", params: { rider: rider_params }
+      post "/api/v1/riders", params: rider_params
 
       expect(response.status).to eq(400)
     end
-    it '400 if no user found' do
+    it '404 if no user found' do
       rider_params = {
         trip_id: @trip.id,
-        user_id: 40928753,
+        email: 'thisemailisfake@woo.com',
         driver: false,
         budget: 300
       }
-      post "/api/v1/riders", params: { rider: rider_params }
+      post "/api/v1/riders", params: rider_params
 
-      expect(response.status).to eq(400)
+      expect(response.status).to eq(404)
     end
-    it '400 if no trip or no user provided' do
+    it '404 if no trip or no user provided' do
       rider_params = {
         driver: false,
         budget: 300
       }
-      post "/api/v1/riders", params: { rider: rider_params }
+      post "/api/v1/riders", params: rider_params
 
-      expect(response.status).to eq(400)
+      expect(response.status).to eq(404)
     end
   end
 end
