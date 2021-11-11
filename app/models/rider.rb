a9cbb239c6b27f9a2f4beb160fd0e6151b27b97c
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-11 00:22:16 -0700

    Remove budget validation

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -2,8 +2,6 @@ class Rider < ApplicationRecord
   belongs_to :trip
   belongs_to :user
 
-  validates :budget, numericality: true
-
   def self.by_user_and_trip(user_id, trip_id)
     where("user_id = ? AND trip_id = ?", user_id, trip_id).first
   end
