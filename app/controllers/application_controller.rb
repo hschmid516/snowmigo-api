c02f04e625080a7fa6c4aa2df97fd19747693e56
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-10 22:27:37 -0700

    Add error responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::API
-  
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :bad_request
+  rescue_from ActiveRecord::StatementInvalid, with: :bad_request
+
   private
 
   def bad_request
     render json: { error: "400 Bad Request" }, status: 400
+  end
+
+  def record_not_found
+   render json: { error: "404 Not Found" }, status: 404
   end
 end
