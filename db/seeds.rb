dbfc59561d78c517a66605ee78b01972518be77d
Laura Court <laura.mcourt@gmail.com>
2021-11-10 20:40:17 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,54 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 User.destroy_all
 Trip.destroy_all
 Rider.destroy_all
 
 users = FactoryBot.create_list(:user, 5)
-trips = FactoryBot.create_list(:trip, 3)
-FactoryBot.create(:rider, user_id: User.first.id, trip_id: Trip.first.id)
-FactoryBot.create(:rider, user_id: User.last.id, trip_id: Trip.first.id)
-FactoryBot.create(:rider, user_id: User.last.id, trip_id: Trip.last.id)
+trip1 = FactoryBot.create(:trip, start_date: '2025-11-02', end_date: '2025-12-12')
+trip2 = FactoryBot.create(:trip, start_date: '2020-11-02', end_date: '2020-12-12')
+FactoryBot.create(:rider, user_id: User.first.id, trip_id: trip1.id)
+FactoryBot.create(:rider, user_id: User.last.id, trip_id: trip1.id)
+FactoryBot.create(:rider, user_id: User.first.id, trip_id: trip2.id)
 FactoryBot.create(:friendship, user_id: users[0].id, friend_id: users[1].id)
 FactoryBot.create(:friendship, user_id: users[1].id, friend_id: users[0].id)
 FactoryBot.create(:friendship, user_id: users[2].id, friend_id: users[3].id)
-FactoryBot.create(:resort_option, trip_id: trips[0].id)
-FactoryBot.create(:resort_option, trip_id: trips[0].id)
-FactoryBot.create(:resort_option, trip_id: trips[1].id)
-# User.create(
-#   name: "Vice Admiral Holdo",
-#   email: "allene_runolfsdottir@hintz.org",
-#   exp_level: 9,
-#   ski_pass: "Epic",
-#   address: "103 Moshe Station, Brandontown, NC 63139",
-#   ski_or_board: 1,
-#   emergency_name: "Quickbeam",
-#   emergency_number: "2813308004"
-# )
-
-# User.create(
-#   name: "Shmi Skywalker",
-#   email: "humberto@reichert.io",
-#   exp_level: 2,
-#   ski_pass: "Epic",
-#   address: "Suite 528 810 Tamara Square, East Ileenport, WY 78509-2304",
-#   ski_or_board: 5,
-#   emergency_name: "Galadriel",
-#   emergency_number: "2813308004"
-# )
-
-# Rider.create(
-#   trip_id: 419,
-#   user_id: User.first.id,
-#   host: false,
-#   driver: false,
-#   budget: 45
-# )
-
-# Trip.create(
-#   resort_id: 1,
-#   start_date: 'Tue, 02 Nov 2021',
-#   end_date: 'Tue, 02 Nov 2021'
-# )
+FactoryBot.create(:resort_option, trip_id: trip1.id)
+FactoryBot.create(:resort_option, trip_id: trip1.id)
+FactoryBot.create(:resort_option, trip_id: trip2.id)
--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -2,7 +2,5 @@ FactoryBot.define do
   factory :trip do
     # association :resort, factory: :resort_option
     name { Faker::Lorem.sentence(word_count: 3) }
-    start_date { "2021-11-02" }
-    end_date { "2021-11-08" }
   end
 end
