cf5fc13afa08e876cfa3f06e6531e3cf0fa8b85a
Henry Schmid <henry.schmid1@gmail.com>
2021-11-11 17:53:58 -0700

    Add vote to rider serializer

--- app/serializers/rider_serializer.rb
+++ app/serializers/rider_serializer.rb
@@ -1,9 +1,5 @@
 class RiderSerializer
   include JSONAPI::Serializer
 
-  attributes :user_id, :trip_id, :host, :driver, :budget, :rider_name
-
-  # attribute :name do |rider|
-  #   rider.name
-  # end
+  attributes :user_id, :trip_id, :host, :driver, :budget, :rider_name, :vote
 end
